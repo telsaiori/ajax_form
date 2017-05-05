b6d097fb7b31796aea6ca1fd353cfdbf77ebe14a
telsa <telsaiori.git@gmail.com>
2017-05-05 15:43:12 +0800

    change to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161115092859) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "memos", force: :cascade do |t|
     t.text     "memo"
     t.datetime "created_at", null: false
